35738739e385ecb7700d86c502d4e21316ecf7ad
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 14:39:32 -0600

    Update fix script

--- fix.sh
+++ fix.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-# Repair the cluster
-gcloud container clusters repair private-cluster-auto \
-    --zone us-west1 \
-    --project=api-for-warp-drive
-
-# Verify status
-gcloud container clusters describe private-cluster-auto \
-    --zone us-west1 \
-    --project=api-for-warp-drive \
-    --format="json(status,statusMessage)"
+IyEvYmluL2Jhc2gKCiMgUmVwYWlyIHRoZSBjbHVzdGVyCmdjbG91ZCBjb250YWluZXIgY2x1c3RlcnMgcmVwYWlyIHByaXZhdGUtY2x1c3Rlci1hdXRvIFwKICAgIC0tem9uZSB1cy13ZXN0MSBcCiAgICAtLXByb2plY3Q9YXBpLWZvci13YXJwLWRyaXZlCgojIFZlcmlmeSBzdGF0dXMKZ2Nsb3VkIGNvbnRhaW5lciBjbHVzdGVycyBkZXNjcmliZSBwcml2YXRlLWNsdXN0ZXItYXV0byBcCiAgICAtLXpvbmUgdXMtd2VzdDEgXAogICAgLS1wcm9qZWN0PWFwaS1mb3Itd2FycC1kcml2ZSBcCiAgICAtLWZvcm1hdD0ianNvbihzdGF0dXMsc3RhdHVzTWVzc2FnZSkiCg==
