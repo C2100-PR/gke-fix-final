808acd746e786febd04c44b47ce8be04a69e15d2
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 14:48:57 -0600

    Restore original repair command

--- fix.sh
+++ fix.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcloud container clusters update private-cluster-auto --zone us-west1 --project=api-for-warp-drive
-gcloud container clusters describe private-cluster-auto --zone us-west1 --project=api-for-warp-drive --format="json(status,statusMessage)"
+gcloud container clusters repair private-cluster-auto --zone us-west1 --project=api-for-warp-drive
