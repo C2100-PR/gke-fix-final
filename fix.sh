3be4788f806f1a7d2f75bda6b0927c1911ef1a8d
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 14:47:25 -0600

    Fix repair command syntax

--- fix.sh
+++ fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 gcloud container clusters repair private-cluster-auto --zone us-west1 --project=api-for-warp-drive
+sleep 2
+gcloud container clusters describe private-cluster-auto --zone us-west1 --project=api-for-warp-drive --format="json(status,statusMessage)"
