efb59aea547a69f2cbb9d1f863e647597292b12d
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 14:39:23 -0600

    Update fix script

--- fix.sh
+++ fix.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo 'Starting GKE cluster repair...'
-gcloud container clusters repair private-cluster-auto --zone us-west1 --project=api-for-warp-drive
+# Repair the cluster
+gcloud container clusters repair private-cluster-auto \
+    --zone us-west1 \
+    --project=api-for-warp-drive
 
-echo 'Verifying status...'
-gcloud container clusters describe private-cluster-auto --zone us-west1 --project=api-for-warp-drive --format="json(status,statusMessage)"
+# Verify status
+gcloud container clusters describe private-cluster-auto \
+    --zone us-west1 \
+    --project=api-for-warp-drive \
+    --format="json(status,statusMessage)"
