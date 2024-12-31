2a28529c7fb23403598b27d2d13eea0fdeec0eb7
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 14:45:17 -0600

    Add complete repair solution

--- fix.sh
+++ fix.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Repair the cluster
 gcloud container clusters repair private-cluster-auto --zone us-west1 --project=api-for-warp-drive
-
-# Verify status
 gcloud container clusters describe private-cluster-auto --zone us-west1 --project=api-for-warp-drive --format="json(status,statusMessage)"
