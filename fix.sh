5db58cf56c4e836ac503bdddb3653e7c4ba4b8f3
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 15:02:41 -0600

    Update script with logging and monitoring parameters

--- fix.sh
+++ fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-gcloud container clusters update private-cluster-auto --zone us-west1 --project=api-for-warp-drive --enable-stackdriver-kubernetes
+gcloud container clusters update private-cluster-auto \
+    --zone us-west1 \
+    --project=api-for-warp-drive \
+    --logging=SYSTEM,WORKLOAD \
+    --monitoring=SYSTEM,WORKLOAD
